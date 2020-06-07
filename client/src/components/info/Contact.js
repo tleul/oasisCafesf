baff4d93ae8c3c2da78149dc7a27710d4cef5975
Tekleselassie Leul <edenlove2019@gmail.com>
2020-06-07 13:39:49 -0700

    changes

--- client/src/components/info/Contact.js
+++ client/src/components/info/Contact.js
@@ -1,122 +1,151 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Contact = () => {
+	const [contactForm, setcontactform] = useState({
+		name: '',
+		email: '',
+		message: '',
+	});
+	const [errors, seterrors] = useState({});
+	const { name, email, message } = contactForm;
+
+	const onChange = (e) => {
+		const validEmailRegex = RegExp(
+			/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+		);
+		e.preventDefault();
+		const { name, value } = e.target;
+		switch (name) {
+			case 'name':
+				errors.name =
+					value.length < 5
+						? 'Name must be more than 5 charcters Long!'
+						: '';
+				break;
+			case 'email':
+				errors.email = validEmailRegex.test(value)
+					? ''
+					: 'email is not valid';
+				break;
+			case 'message':
+				errors.message =
+					value.length < 5 ? 'Please try to describe more ' : '';
+				break;
+			default:
+				break;
+		}
+
+		setcontactform({ ...contactForm, [e.target.name]: e.target.value });
+	};
+	const sendEmail = (e) => {
+		e.preventDefault();
+		console.log(errors);
+	};
+
 	return (
 		<Fragment>
-			<div className='all-page-title page-breadcrumb'>
+			{/* <div className='all-page-title page-breadcrumb'>
 				<div className='container text-center'>
+					<div className='row'></div>
+				</div>
+			</div> */}
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<div className='contact-box'>
+				<div className='container'>
 					<div className='row'>
 						<div className='col-lg-12'>
-							<h1>Contact</h1>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div className='map-full'>
-				<div className='contact-box'>
-					<div className='container'>
-						<div className='row'>
-							<div className='col-lg-12'>
-								<div className='heading-title text-center'>
-									<h2>Contact</h2>
-									<p>
-										Lorem Ipsum is simply dummy text of the
-										printing and typesetting
-									</p>
-								</div>
+							<div className='heading-title text-center'>
+								<h2>Contact</h2>
+								<p>
+									Please type any thing you feel, We are
+									welcome to assist you!!
+								</p>
 							</div>
 						</div>
-						<div className='row'>
-							<div className='col-lg-12'>
-								<form id='contactForm'>
-									<div className='row'>
-										<div className='col-md-12'>
-											<div className='form-group'>
-												<input
-													type='text'
-													className='form-control'
-													id='name'
-													name='name'
-													placeholder='Your Name'
-													required
-													data-error='Please enter your name'
-												/>
-												<div className='help-block with-errors'></div>
-											</div>
-										</div>
-										<div className='col-md-12'>
-											<div className='form-group'>
-												<input
-													type='text'
-													placeholder='Your Email'
-													id='email'
-													className='form-control'
-													name='name'
-													required
-													data-error='Please enter your email'
-												/>
-												<div className='help-block with-errors'></div>
-											</div>
-										</div>
+					</div>
+					<div className='row'>
+						<div className='col-lg-12'>
+							<form id='contactForm'>
+								<div className='row'>
+									<div className='col-md-12'>
+										<div className='form-group'>
+											<input
+												type='text'
+												className='form-control'
+												id='name'
+												name='name'
+												value={name}
+												onChange={(e) => onChange(e)}
+												placeholder='Your Name'
+												required
+												data-error='Please enter your name'
+											/>
 
-										<div className='col-md-12'>
-											<div className='form-group'>
-												<textarea
-													className='form-control'
-													id='message'
-													placeholder='Your Message'
-													rows='4'
-													data-error='Write your message'
-													required></textarea>
-												<div className='help-block with-errors'></div>
-											</div>
-											<div className='submit-button text-center'>
-												<button
-													className='btn btn-common'
-													id='submit'
-													type='submit'>
-													Send Message
-												</button>
-												<div
-													id='msgSubmit'
-													className='h3 text-center hidden'></div>
-												<div className='clearfix'></div>
+											<div className='help-block with-errors'>
+												<p style={{ color: 'red' }}>
+													{errors.name}
+												</p>
 											</div>
 										</div>
 									</div>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
+									<div className='col-md-12'>
+										<div className='form-group'>
+											<input
+												type='text'
+												placeholder='Your Email'
+												id='email'
+												className='form-control'
+												name='email'
+												onChange={(e) => onChange(e)}
+												value={email}
+												required
+												data-error='Please enter your email'
+											/>
+											<div className='help-block with-errors'>
+												<p style={{ color: 'red' }}>
+													{errors.email}
+												</p>
+											</div>
+										</div>
+									</div>
 
-				<div className='contact-imfo-box'>
-					<div className='container'>
-						<div className='row'>
-							<div className='col-md-4'>
-								<i className='fa fa-volume-control-phone'></i>
-								<div className='overflow-hidden'>
-									<h4>Phone</h4>
-									<p className='lead'>+01 123-456-4590</p>
+									<div className='col-md-12'>
+										<div className='form-group'>
+											<textarea
+												className='form-control'
+												id='message'
+												placeholder='Your Message'
+												rows='4'
+												onChange={(e) => onChange(e)}
+												name='message'
+												value={message}
+												data-error='Write your message'
+												required></textarea>
+											<div className='help-block with-errors'>
+												<p style={{ color: 'red' }}>
+													{errors.message}
+												</p>
+											</div>
+										</div>
+										<div className='submit-button text-center'>
+											<button
+												onClick={(e) => sendEmail(e)}
+												className='btn btn-common'
+												type='submit'>
+												Send Message
+											</button>
+											<div
+												id='msgSubmit'
+												className='h3 text-center hidden'></div>
+											<div className='clearfix'></div>
+										</div>
+									</div>
 								</div>
-							</div>
-							<div className='col-md-4'>
-								<i className='fa fa-envelope'></i>
-								<div className='overflow-hidden'>
-									<h4>Email</h4>
-									<p className='lead'>yourmail@gmail.com</p>
-								</div>
-							</div>
-							<div className='col-md-4'>
-								<i className='fa fa-map-marker'></i>
-								<div className='overflow-hidden'>
-									<h4>Location</h4>
-									<p className='lead'>
-										800, Lorem Street, US
-									</p>
-								</div>
-							</div>
+							</form>
 						</div>
 					</div>
 				</div>
