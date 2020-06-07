d7360f3850897ecf292632157496c64d3a36b5f5
Tekleselassie Leul <edenlove2019@gmail.com>
2020-06-06 20:29:18 -0700

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	NavLink,
+} from 'react-router-dom';
+import Menu from './components/menu/Menu';
+import Home from './components/layout/Home';
+import Footer from './components/layout/Footer';
+import Contact from './components/info/Contact';
+import Gallery from './components/Gallery';
 
-import './App.css';
-
-function App() {
-	return (
-		<div className='App'>
-			<h1>Oasis</h1>
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<Router>
+				<Navbar />
+				<Route path='/' component={Home} />
+				<Fragment>
+					<Switch>
+						<Route path='/menu' component={Menu} />
+						<Route path='/contact' component={Contact} />
+						<Route path='/gallery' component={Gallery} />
+					</Switch>
+				</Fragment>
+				<Footer />
+			</Router>
+		);
+	}
 }
 
 export default App;
