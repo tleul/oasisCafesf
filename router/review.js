54f7656f3020b91a9d325d5f7b51ab77cdda45b3
Tekleselassie Leul <edenlove2019@gmail.com>
2020-06-27 17:22:06 -0700

    review

--- router/review.js
+++ router/review.js
@@ -4,9 +4,8 @@ const router = express.Router();
 const fs = require('fs');
 const fileName = 'user.json';
 const db = require('../user.json');
-<<<<<<< HEAD
 //Post Review
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 	const { name, review, rate } = req.body;
 	const id = uuidv4();
 	const data = {
@@ -15,42 +14,16 @@ router.post('/', (req, res) => {
 		review: review,
 		rate: rate,
 	};
-	console.log(data);
-
-	// await fs.writeFile(fileName, JSON.stringify(db), function writeJSON(err) {
-	// 	if (err) return console.log(err);
-	//     console.log(db)
-	// });
-});
-=======
-//Post Review 
-router.post ('/', async(req,res)=>{
-    const {name, review, rate} = req.body
-    const id = uuidv4();
-    const data = {
-        id:id,
-        name: name ,
-        review: review,
-        rate:rate
-    }
-    db.review.push(data);
- await fs.writeFile(fileName, JSON.stringify(db), function writeJSON(err) {
+	db.review.push(data);
+	await fs.writeFile(fileName, JSON.stringify(db), function writeJSON(err) {
 		if (err) return console.log(err);
-        console.log(db)
+		console.log(db);
 	});
-
-
-
-	
-})
-router.get('/', (req,res) => {
-
-    const reviews = db.review 
-    const filterReview = reviews.filter(review => review.rate > 3)
-    res.json(filterReview)
-
-})
-
->>>>>>> bceac6184d456d010a82147e418d0ef0435e95ab
+});
+router.get('/', (req, res) => {
+	const reviews = db.review;
+	const filterReview = reviews.filter((review) => review.rate > 3);
+	res.json(filterReview);
+});
 
 module.exports = router;
